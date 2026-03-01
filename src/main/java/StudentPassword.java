facce561b89836ad0f9f00269f3cb15bae143c10
JKBoy2244 <jobayer2006@gmail.com>
2026-03-01 22:40:43 +0000

    Create StudentPassword.java

---
+++ src/main/java/StudentPassword.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class StudentPassword {
+  static ArrayList<String> studentPasswords = new ArrayList<String>();
+}
