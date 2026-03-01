90d0c8fa35f83dbb5095500052e944e1771d1d3e
JKBoy2244 <jobayer2006@gmail.com>
2026-03-01 22:38:47 +0000

    Create ECApplicationOutcomes.java

---
+++ src/main/java/ECApplicationOutcomes.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class ECApplicationOutcomes {
+
+  static LinkedHashSet<List<String>> Outcomes = new LinkedHashSet<List<String>>();
+}
